571206e946305b6f4ec2105062ddfe1128ef8c8f
Islam Ibrahim Abd El Naby Abd El daim <islam.ibrahim_pg@alexu.edu.eg>
2025-06-12 17:08:49 +0300

    fix: correct cartItems schema type

--- models/User.js
+++ models/User.js
@@ -1,29 +1,28 @@
 import mongoose from "mongoose";
-import { object } from "prop-types";
 
 const userSchema = new mongoose.Schema({
-   _id:{
+  _id: {
     type: String,
     required: true
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    imageUrl: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    cartItems: {
-        type: object,
-        default: {},
-    }
-}, {minimize: false});
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  cartItems: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {},
+  }
+}, { minimize: false });
 
 const User = mongoose.models.user || mongoose.model("User", userSchema);
 export default User;
