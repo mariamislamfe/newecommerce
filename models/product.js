4cee625268f7e4bffd7be49ed101691a44290508
Islam Ibrahim Abd El Naby Abd El daim <islam.ibrahim_pg@alexu.edu.eg>
2025-06-17 02:27:03 +0300

    yaraaaabbbb

--- models/product.js
+++ models/product.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
+  userId: { type: String, required: true, ref: "User" }, // خلي الـ ref = اسم الموديل User لو موجود
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: Array, required: true },
+  price: { type: Number, required: true },
+  offerPrice: { type: Number, required: true },
+  category: { type: String, required: true },
+  date: { type: Number, required: true },
+});
 
-    userId: { type: String, required: true, ref: "user" },
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    image: { type: Array, required: true },
-    price: { type: Number, required: true },
-    offerPrice: { type: Number, required: true },
-    category: { type: String, required: true },
-    date: { type: Number, required: true },
-})
+// هنا نعمل check قبل التسجيل عشان نتفادى errors وقت hot-reload
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
 
-const Product = mongoose.models.Product || mongoose.model("Product", productSchema)
-
-export default Product
+export default Product;
