8e9fa0198f9a069bcfff6545569fde8da51d2269
Islam Ibrahim Abd El Naby Abd El daim <islam.ibrahim_pg@alexu.edu.eg>
2025-06-16 21:44:22 +0300

    save

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -5,38 +5,44 @@ import User from "@/models/User";
 import { inngest } from "@/config/inngest";
 
 export async function POST(request) {
-    try {
-        const {userId} = getAuth(request)
-        const {address} = await request.json()
-        if (!address || item.length === 0) {
-            return NextResponse.json({success: false, message: "invalid data"})
-        }
+  try {
+    const { userId } = getAuth(request);
+    const { address, items } = await request.json();
 
-        // calculate amount
-        const amount = item.reduce(async (acc, curr) => {
-            const product = await Product.findById(item.product)
-            return acc + product.offerPrice * item.quantity;
-        },0)
-            await inngest.send({
-        
-                    name: "order/created",
-                    data: {
-                        userId,
-                        items,
-                        address,
-                        amount: amount + Math.floor(amount * 0.02),
-                        date: Date.now()
-                    }
-            })
-
-            // clear cart
-            const user = await User.findById(userId)
-            user.cartItems = {}
-            await user.save()
-            return NextResponse.json({success: true, message: 'order placed successfully'})
-            
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({success: false, message: error.message})
+    if (!address || items.length === 0) {
+      return NextResponse.json({ success: false, message: "invalid data" });
     }
+
+    // calculate amount
+    let amount = 0;
+    for (const item of items) {
+      const product = await Product.findById(item.productId);
+      amount += product.offerPrice * item.quantity;
+    }
+
+    // send order event to inngest
+    await inngest.send({
+      name: "order/created",
+      data: {
+        userId,
+        items,
+        address,
+        amount: amount + Math.floor(amount * 0.02),
+        date: Date.now(),
+      },
+    });
+
+    // clear cart
+    const user = await User.findById(userId);
+    user.cartItems = {};
+    await user.save();
+
+    return NextResponse.json({
+      success: true,
+      message: "order placed successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ success: false, message: error.message });
+  }
 }
